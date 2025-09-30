3f65921532b07f693b950f4fd787645464e183c5
Agustin Quiroz <agustin.quiroz@capitalexpress.cl>
2025-09-30 16:08:13 -0300

    refactor: remove test configuration from Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['src/test/setup.ts'],
-    globals: true,
-    css: true,
-    coverage: {
-      reporter: ['text', 'html'],
-    },
-  },
+  base: '/weekly-roster-v2/',
 });
 
 
